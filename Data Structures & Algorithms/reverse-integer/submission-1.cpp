f8390e4d72aa943bc4e172e99efa8c2723fd9e94
Suryansh Pundir <suryanshpundir0001@gmail.com>
2026-06-26 21:38:45 +0530

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.cpp
@@ -0,0 +1,20 @@
+//jai SitaRam
+class Solution {
+public:
+    int reverse(int x) {
+
+        long long rev = 0;
+
+        while(x != 0){
+
+            int ld = x % 10;
+            rev = rev * 10 + ld;
+            x /= 10;
+        }
+
+        if(rev > INT_MAX || rev < INT_MIN)
+            return 0;
+
+        return (int)rev;
+    }
+};
