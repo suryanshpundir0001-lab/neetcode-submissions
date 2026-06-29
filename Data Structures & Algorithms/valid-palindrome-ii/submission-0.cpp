cb7d32e06f7dd05ad24e1199187d11000134d771
Suryansh Pundir <suryanshpundir0001@gmail.com>
2026-06-29 21:38:10 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,27 @@
+//jai SitaRam
+class Solution {
+public:
+    bool check(string &s, int i, int j){
+        while(i < j){
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+
+        while(i < j){
+            if(s[i] != s[j]){
+                return check(s, i + 1, j) || check(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};
