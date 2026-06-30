3a23e8bee0865afab3c26a2c55f697bf24845998
Suryansh Pundir <suryanshpundir0001@gmail.com>
2026-06-30 10:18:22 +0530

    Add: linked-list-cycle-detection - submission-4

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-4.cpp
@@ -0,0 +1,17 @@
+//jai SitaRam
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if(head==NULL || head->next==NULL) return false;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast!=nullptr && fast->next!=NULL){
+            slow = slow-> next;
+            fast = fast->next->next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+};
