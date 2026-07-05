cbe60bf7f2cc8acd81b5bc7e50d0e990caccb75d
Suryansh Pundir <suryanshpundir0001@gmail.com>
2026-07-05 06:47:59 +0530

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.cpp
@@ -0,0 +1,17 @@
+//jai SitaRam
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+    
+        while(curr!=nullptr){
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+};
